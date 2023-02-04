0b0444bc96dd8b04fd5908629b82210efc001928
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-02-03 19:45:01 -0700

    Compiled successfully and deployed basic smoketest.

--- src/contracts/Mint.sol
+++ src/contracts/Mint.sol
@@ -2,27 +2,7 @@
 pragma solidity ^0.5.0;
 
 contract Mint {
-    // the owner of the contract
-    address public owner;
-    // the address of the token contract
-    address public tokenAddress;
-    // the amount of tokens to mint
-    uint256 public amount;
-
-    // constructor
-    constructor(address _tokenAddress, uint256 _amount) public {
-        owner = msg.sender;
-        tokenAddress = _tokenAddress;
-        amount = _amount;
-    }
-
-    // // mint tokens
-    // function mint() public {
-    //     // check if the caller is the owner
-    //     require(msg.sender == owner, "Only the owner can mint tokens");
-    //     // mint the tokens
-    //     ERC20(tokenAddress).mint(amount);
-    // }
+    string public name = "AI Minting Contract";
 }
 
 
