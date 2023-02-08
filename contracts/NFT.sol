0786e2fe5506cd963e0cdd4ca99796d4150c07c8
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-02-08 07:12:12 -0700

    Added imports for ERC721

---
+++ contracts/NFT.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
--- src/contracts/Mint.sol
+++
@@ -1,8 +0,0 @@
-// minting contract
-pragma solidity ^0.5.0;
-
-contract Mint {
-    string public name = "AI Minting Contract";
-}
-
-
